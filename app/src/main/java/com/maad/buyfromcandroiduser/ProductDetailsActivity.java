e7ee6188ab51e07411d9260d0b305e102a81e57a
AhmedMaad <atef.ahmed1341@gmail.com>
2021-04-26 18:20:41 +0200

    product details activity is done

--- app/src/main/java/com/maad/buyfromcandroiduser/ProductDetailsActivity.java
+++ app/src/main/java/com/maad/buyfromcandroiduser/ProductDetailsActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
@@ -16,11 +17,18 @@ public class ProductDetailsActivity extends AppCompatActivity {
 
         ProductModel product = getIntent().getParcelableExtra("product");
 
-
         String imageTransitionName = getIntent().getStringExtra("imageTransition");
         ImageView productIV = findViewById(R.id.iv_product);
         productIV.setTransitionName(imageTransitionName);
         Glide.with(this).load(product.getImage()).into(productIV);
+
+        TextView titleTV = findViewById(R.id.tv_title);
+        TextView descriptionTV = findViewById(R.id.tv_description);
+        TextView priceTV = findViewById(R.id.tv_price);
+
+        titleTV.setText(product.getTitle());
+        descriptionTV.setText(product.getDescription());
+        priceTV.setText(String.valueOf(product.getPrice()));
 
     }
 }
